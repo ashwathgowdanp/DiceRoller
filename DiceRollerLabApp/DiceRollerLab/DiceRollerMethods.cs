db3f4ca1b7e7d0eaa455766ba5748c1682cb64a4
ashwathgowdanp <ashwathgowdanp@gmail.com>
2023-01-24 17:37:40 -0500

    Dice Roller Project Submission - Initial Version

---
+++ DiceRollerLabApp/DiceRollerLab/DiceRollerMethods.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRollerLab
+{
+    internal class DiceRollerMethods
+    {
+        public static int GenerateRandomNumber(int DiceSide) 
+        {            
+            Random rand = new Random();           
+            {
+                int myRandomNum = rand.Next(1,DiceSide);                
+                return myRandomNum;
+            }
+        }
+        public static string ReturnValidCombination(int DiceSide1, int DiceSide2)
+        {
+            if (DiceSide1 == 1 && DiceSide2 == 1)
+            {
+                return "Snake Eyes: Two 1s";
+            }
+            else if(DiceSide1 == 1 && DiceSide2 == 2)
+            {
+                return "Ace Deuce";
+            }
+            else if (DiceSide1 == 6 && DiceSide2 == 6)
+            {
+                return "Box Cars";
+            }
+            else
+                return "";
+            
+        }
+
+        public static string ReturnValidTotal(int DiceSide1, int DiceSide2)
+        {
+            int TotalNumber = DiceSide1 + DiceSide2;
+            if (TotalNumber == 7 || TotalNumber == 11)
+            {
+                return "Win!";
+            }
+            else if (TotalNumber == 2 || TotalNumber == 3 || TotalNumber == 12)
+            {
+                return "Craps:";
+            }
+            else
+               return "";
+
+        }
+        public static string ReturnValidCombinationForOthers(int DiceSide1, int DiceSide2)
+        {
+            if (DiceSide1 == 2 && DiceSide2 == 6)
+            {
+                return "Easy Eight";
+            }
+            else if (DiceSide1 == 7 && DiceSide2 == 3)
+            {
+                return "Big one on the end";
+            }
+            else if (DiceSide1 == 6 && DiceSide2 == 6)
+            {
+                return "MidNight";
+            }
+            else
+                return "";
+
+        }
+
+        public static string ReturnValidTotalForOthers(int DiceSide1, int DiceSide2)
+        {
+            int TotalNumber = DiceSide1 + DiceSide2;
+            if (TotalNumber == 11 || TotalNumber == 16)
+            {
+                return "Win!";
+            }
+            else if (TotalNumber == 6 || TotalNumber == 8 || TotalNumber == 4)
+            {
+                return "Craps:";
+            }
+            else
+                return "";
+
+        }
+    }
+}
---
+++ DiceRollerLabApp/DiceRollerLab/Program.cs
@@ -0,0 +1,56 @@
+﻿// Dice Roller
+using DiceRollerLab;
+
+int Counter = 1;
+
+Console.WriteLine("Welcome to the Grand Circus Casino");
+Console.Write("How many sides should each side have?:  ");
+
+string? NumberOfSides = Console.ReadLine();
+bool ValidNumber = int.TryParse(NumberOfSides, out int ParsedInt);
+if (!ValidNumber)
+{
+    Console.WriteLine("Please enter a valid Number: Please try again Later");
+}
+bool TryAgain = false;
+do
+{
+    int FirstRandomNumber = DiceRollerMethods.GenerateRandomNumber(ParsedInt + 1);
+    int SecondRandomNumber = DiceRollerMethods.GenerateRandomNumber(ParsedInt + 1);
+    int Sum = FirstRandomNumber + SecondRandomNumber;
+    Console.WriteLine($"Roll {Counter}: ");
+    Console.WriteLine($"You rolled a {FirstRandomNumber} and a {SecondRandomNumber} ({Sum} total)");
+    if (ParsedInt == 6) // When user inputs 6 as dice input
+    {
+        string ResultOfCombination = DiceRollerMethods.ReturnValidCombination(FirstRandomNumber, SecondRandomNumber);
+        string ValidTotal = DiceRollerMethods.ReturnValidTotal(FirstRandomNumber, SecondRandomNumber);
+        Console.WriteLine($"{ResultOfCombination}");
+        Console.WriteLine($"{ValidTotal}");
+    }
+
+    else
+    {
+        string ResultOfCombination = DiceRollerMethods.ReturnValidCombinationForOthers(FirstRandomNumber, SecondRandomNumber);
+        string ValidTotal = DiceRollerMethods.ReturnValidTotalForOthers(FirstRandomNumber, SecondRandomNumber);
+        Console.WriteLine($"{ResultOfCombination}");
+        Console.WriteLine($"{ValidTotal}");
+    }
+              
+
+
+    Console.Write("Roll Again? (y/n):");
+    string? RollAgain = Console.ReadLine();
+    if (RollAgain != null && RollAgain.ToLower() == "y")
+    {
+        Counter = Counter + 1;
+        TryAgain = true;
+        Sum = 0;
+    }
+    else
+    {
+        Console.WriteLine("Thanks for Playing!!");
+        TryAgain = false;
+    }
+
+}
+while (TryAgain);
